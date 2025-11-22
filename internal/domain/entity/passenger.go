aa8a67c6b15bff9b0ba227af3bb855dd6efa208f
Mani Zamani <mani.84.zmn@gmail.com>
2025-11-22 13:59:55 +0330

    fix: fix the bug of the passenger

--- internal/domain/entity/passenger.go
+++ internal/domain/entity/passenger.go
@@ -3,15 +3,17 @@ package entity
 import (
 	"time"
 
+	"github.com/Manizmn84/GoTravel/internal/domain/enum"
 	"gorm.io/gorm"
 )
 
 type Passenger struct {
-	ID        uint `gorm:"primarykey"`
+	ID        uint `gorm:"primaryKey"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 	Password  string
 	Username  string
-	email     string `gorm:"email"`
+	Email     string `gorm:"type:varchar(100);uniqueIndex"`
+	Sex       enum.Gender
 }
